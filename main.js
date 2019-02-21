68088d296a8513621b17c944006b4e83291ebbfa
Pierce Collazo <pierce.collazo@codeimmersive.com>
2019-02-21 11:29:11 -0500

    Title case conversion functunal.

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ window.onload = init;
 
 function init() {
     document.querySelector('#input')
-        .addEventListener('keydown', handleTyping);
+        .addEventListener('keyup', handleTyping);
 }
 
 function handleTyping(event) {
-    let userInput = event.target.value;
-    let answer = userInput;
-    
-    document.querySelector('#changeme').innerText = answer;
+    let userInput = event.target.value.toString();
+    // let answer = userInput;
+    let myString = userInput.toLowerCase().split(' ');
+
+    for(let i = 0; i < myString.length; i++){
+        myString[i] = myString[i].charAt(0).toUpperCase() + myString[i].slice(1);
+    }
+
+    document.querySelector('#changeme').innerText = myString.join(' ')
 }
